2c9cac4fa790503171b938eadda85f2b9fef04c9
jhpsantos <joao.henrique.m.p.s@gmail.com>
2018-08-28 12:11:00 -0300

    Subindo e criando classes vazias para o DES de Usuario;

---
+++ Patrimonio.Business/UsuarioBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patrimonio.Business
+{
+    class UsuarioBusiness
+    {
+    }
+}
---
+++ Patrimonio.Repository/RepositoriosEntidades/UsuarioRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patrimonio.Repository.RepositoriosEntidades
+{
+    class UsuarioRepository
+    {
+    }
+}
